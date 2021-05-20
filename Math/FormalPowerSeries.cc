e44b3fddb2f20a0af3a05e17e1e279442e85c9a7
Takata1128 <t2130072@edu.cc.uec.ac.jp>
2021-05-20 17:39:02 +0900

    fix fps

--- Math/FormalPowerSeries.cc
+++ Math/FormalPowerSeries.cc
@@ -118,7 +118,7 @@ template <typename Mint> struct FormalPowerSeries : public vector<Mint> {
     }
 
     // multiply and divide (1+cz^d)
-    void multiply(const int d, const T c) {
+    void multiply(const int d, const Mint c) {
         int n = this->size();
         if(c == T(1))
             for(int i = n - d; i >= 0; i--)
@@ -130,7 +130,7 @@ template <typename Mint> struct FormalPowerSeries : public vector<Mint> {
             for(int i = n - d; i >= 0; i--)
                 (*this)[i + d] += (*this)[i] * c;
     }
-    void divide(const int d, const T c) {
+    void divide(const int d, const Mint c) {
         int n = this->size();
         if(c == T(1))
             for(int i = 0; i < n - d; i++)
